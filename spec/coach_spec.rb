0b05e369f2d3226711de5200e0455d0420459b7f
jamesascarter <jamesascarter@gmail.com>
2014-12-15 13:44:43 +0000

    refactored coach tests

--- spec/coach_spec.rb
+++ spec/coach_spec.rb
@@ -4,7 +4,7 @@ describe Coach do
 
 	let(:coach) {Coach.new}
 	let(:passenger) {double :passenger}
-	let(:station) {double :station}	
+	let(:station) {double :station, :leave => nil}	
 
 
 	it "should have a default capacity of 40" do
@@ -12,24 +12,21 @@ describe Coach do
 	end
 
 	it 'should allow passenger to enter the coach' do
-		allow(station).to receive(:leave)
 		coach.embark(passenger,station)
 		expect(coach.passenger_count).to eq(1)
 	end
 
 	it "should know when it is full" do
-		allow(station).to receive(:leave)
 		40.times{coach.embark(passenger,station)}
 		expect(coach.full?).to eq(true)
 	end
 
 	it "should return an error when the coach is full" do
-		allow(station).to receive(:leave)
 		40.times {coach.embark(passenger,station)}
 		expect(lambda{coach.embark(passenger,station)}).to raise_error("sorry the coach is full")
 	end
 
 	it "should allow passengers to leave a coach" do
-		
+
 	end
 end
